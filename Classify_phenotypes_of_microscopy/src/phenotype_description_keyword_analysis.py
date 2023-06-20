5e05f55cbbb719e1b6df4d7a3a8d19b1ed68488d
Yusheng Yang <yangyusheng12323@outlook.com>
2023-06-20 15:29:48 +0800

    description keyword analysis

---
+++ Classify_phenotypes_of_microscopy/src/phenotype_description_keyword_analysis.py
@@ -0,0 +1,22 @@
+"""
+This script will analyze the keywords in the phenotype descriptions for better understanding of the phenotype description.
+"""
+# %%
+import pyforest
+
+formatted_phenotypes = pd.read_excel("../data/1_formatted/Hayles_2013_OB_formatted_phenotypes.xlsx")
+
+phenotype_description_keywords = formatted_phenotypes["Deletion mutant phenotype description"].str.split(expand=True)\
+                                        .stack()\
+                                            .rename("Count")\
+                                                .droplevel(1, axis=0)\
+                                                    .str.strip(",")\
+                                                        .str.strip()\
+                                                            .value_counts()\
+                                                                .rename_axis("Keyword")
+
+outputFolder = Path("../data/2_phenotype_description_keyword_analysis")
+outputFolder.mkdir(exist_ok=True)
+
+phenotype_description_keywords.to_excel(outputFolder/"Hayles_2013_OB_phenotype_description_keywords.xlsx")
+# %%
