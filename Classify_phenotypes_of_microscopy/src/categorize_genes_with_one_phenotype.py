2db73c5806616bc15c0c3075483169a5093e3449
Yusheng Yang <yangyusheng12323@outlook.com>
2023-06-20 14:23:28 +0800

    reorganize the functions

--- Classify_phenotypes_of_microscopy/src/categorize_genes_with_one_phenotype.py
+++ Classify_phenotypes_of_microscopy/src/categorize_genes_with_one_phenotype.py
@@ -1,4 +1,3 @@
-# %%
 """
 This script will category the genes with one phenotype to different levels of growth phenotypes.
 """
@@ -9,42 +8,42 @@ This script will category the genes with one phenotype to different levels of gr
 import pandas as pd
 from pathlib import Path
 
+# function for category the genes with one phenotype to different levels of growth phenotypes
+def category_one_phenotype(phenotype):
+    if ("spores" in phenotype) and ("germinated" not in phenotype):
+        return "spores"
+    elif ("germinated" in phenotype) and ("spores" in phenotype) and (("divide" not in phenotype) and ("division" not in phenotype)):
+        return "germinated"
+    elif ("germinated" in phenotype) and ("spores" in phenotype) and (("divide" in phenotype) or ("division" in phenotype)):
+        return "germinated_and_divided"
+    elif "microcolonies" in phenotype:
+        return "microcolonies"
+    elif ("very small colonies" in phenotype):
+        return "very small colonies"
+    elif ("small colonies" in phenotype):
+        return "small colonies"
+    else:
+        return "WT"
+    
+# pivot function
+def pivot_analysis_of_the_categories(df, row_name, col_name):
+    pivot_result = df[[row_name, col_name]]\
+                        .value_counts()\
+                            .rename("Count")\
+                                .reset_index()\
+                                    .pivot(index=row_name, columns=col_name, values="Count")\
+                                        .fillna(0)\
+                                            .astype(int)
+    
+    return pivot_result
 
 def main():
     # load the genes with one phenotype
     genes_with_one_phenotype = pd.read_excel("../data/2_grouped_genes/Hayles_2013_OB_grouped_genes.xlsx", sheet_name="One basic phenotype")
 
-    # function for category the genes with one phenotype to different levels of growth phenotypes
-    def category_one_phenotype(phenotype):
-        if ("spores" in phenotype) and ("germinated" not in phenotype):
-            return "spores"
-        elif ("germinated" in phenotype) and ("spores" in phenotype) and (("divide" not in phenotype) and ("division" not in phenotype)):
-            return "germinated"
-        elif ("germinated" in phenotype) and ("spores" in phenotype) and (("divide" in phenotype) or ("division" in phenotype)):
-            return "germinated_and_divided"
-        elif "microcolonies" in phenotype:
-            return "microcolonies"
-        elif ("very small colonies" in phenotype):
-            return "very small colonies"
-        elif ("small colonies" in phenotype):
-            return "small colonies"
-        else:
-            return "WT"
-        
     genes_with_one_phenotype["Category"] = genes_with_one_phenotype["Basic phenotype"].apply(category_one_phenotype)
 
     # pivot the table
-    def pivot_analysis_of_the_categories(df, row_name, col_name):
-        pivot_result = df[[row_name, col_name]]\
-                            .value_counts()\
-                                .rename("Count")\
-                                    .reset_index()\
-                                        .pivot(index=row_name, columns=col_name, values="Count")\
-                                            .fillna(0)\
-                                                .astype(int)
-        
-        return pivot_result
-
     Phenotypes_pivot = pivot_analysis_of_the_categories(genes_with_one_phenotype, "Deletion mutant phenotype description", "Category")
     Essentiality_pivot = pivot_analysis_of_the_categories(genes_with_one_phenotype, "Gene dispensability. This study", "Category")
     Classification_pivot = pivot_analysis_of_the_categories(genes_with_one_phenotype, "Phenotypic classification used for analysis", "Category")
@@ -67,6 +66,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# %%
